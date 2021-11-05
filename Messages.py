04ff5e4b621a97e5d653ae6654107193ea0f235a
harisn5181 <91597885+harisn5181@users.noreply.github.com>
2021-11-05 23:26:39 +0530

    Add files via upload

---
+++ Messages.py
@@ -0,0 +1,34 @@
+m_start =  '            PARADISE CHAT BOT   \nOpen your world with Paradise Chat bot-Meet new people from India to Algeria😁, America to Brazil😊, Indonesia to Africa😚, Thailand to Russia😎, Canada to South America😃, and more🤩🤩🤩.\n\n\nYou can learn different cultures and make real friendship💛💛💛.The best place to have a safe travel around the world without telling who your are 🦠🦠🦠 .\n\n\nParadise Chat Bot connect  people from any places 🌍. Lets explore our 🗺 map 🗺\n\n\n❤️In paradise chat bot we will not allow anyone to flirt others😳, in every country flirting without permission is considered as bad😱, we want everyone a safe enviornment like paradise💜💜,so paradise bot will block  bad contents🚫❌🚫. More details u can see the /rules ,Dont flirt ur partner and be respectful🟢🟢🟢\n\n\nLETS CONNECT THIS AMAZING 🏴🏴‍☠️🏁🚩🏳️‍🌈🏳️‍⚧️🇺🇳🇦🇫🇦🇮🇦🇴🇦🇩🇦🇸🇦🇹🇧🇪🇧🇼🇧🇦🇬🇱🇱🇧🇳🇨🇷🇴🇹🇲🇦🇪 WORLD🪙 THROUGH YOUR FRIENDS😇😇'                                               
+m_is_not_free_users ='Sorry, but there are no free users at the moment'\
+                   ' connecting to your travelling crew.......\n As soon as another user logs in, we will connect you! \nWait here untill we connect sutable match for you          '
+m_is_connect ='The connection has been established. Say hello to your travelling crew! \n/start-click here to travel the world           \n/stop-stop the travelling'
+m_play_again = '\n/start-click here to travel the world           \n/stop-stop the travelling'
+
+m_is_not_user_name = 'You need a username for making a travel from one place to another,GO TO YOUR TELEGRAM PROFILE  AND ADD A USERNAME,THEN COME BACK AND TRAVEL THE WORLD THROGUH PARADISE CHAT BOT'
+m_good_bye = 'Bot:''Bot: we will be glad to see you again!\n/start-click here to travel the world           \n/stop-stop the travelling'
+
+m_disconnect_user = 'Bot: Your parnter has stopped\n\n/start-click here to travel the world           \n/stop-stop the travelling'
+
+m_failed ='Bot: An error has occurred!\n\n/start-click here to travel the world           \n/stop-stop the travelling'
+
+m_like ='Bot: Great choice!'
+
+m_dislike_user ='Bot: Dialogue ended ,press new chat to search next partner'
+
+m_dislike_user_to = 'Bot: partner did not like you, sorry\n\n/start-click here to travel the world           \n/stop-stop the travelling'
+m_send_some_messages = 'Bot: you cannot forward your own messages'
+
+m_has_not_dialog = 'You are not in dialogue\n\n/start-click here to travel the world           '
+
+dislike_str = '\U0001F44E next parnter.'
+
+like_str = '\U0001F44D share id then conversation will end'
+violation_partner_msg='ATTENTION❗️❕❗️❕❗️These words are forbidden to use here. ❌ Flirting ❌ without permission is bad in every country😱😱😱. We want paradise chat bot to be a paradise for everyone 😇😇😇'
+violation_user_msg="Your partner is trying to talk with 🤬forbidden words🤬, We have blocked the 🚧words🚧. If u don't like your partner❌, you can find new partner by New Chat 😊😊😊.We want paradise chat bot to be a paradise for everyone😇😇😇"
+sharelink_user_msg="Your id is succesfully shared"
+error_sharelink_msg="We have issue with sharing the username of Telegram"
+photo_partner_msg=" Buy permission from your partner inorder to send images"
+photo_user_msg="Your partner is trying to send image,you need to give him permsion inorder to see. the photos"
+
+def m_all_like(x):
+    return 'Your crew liked you\n' + 'His username:'+'@'+ str (x)
