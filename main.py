030d43a423dac5639f82a5bbf4dc67a33f0b2697
harisn5181 <91597885+harisn5181@users.noreply.github.com>
2021-11-11 01:42:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ from telebot import types
 from Messages import *
 from dataEgine import *
 
-
-
 API_KEY ='1908250775:AAFoQFc2_qQbl8VhVxwm9m88nsSwOQVNHyI'
+API_KEY = os.getenv('API_KEY')
+bot = telebot.TeleBot(API_KEY)
+
+
 bot = telebot.TeleBot(token=API_KEY)
 
 
