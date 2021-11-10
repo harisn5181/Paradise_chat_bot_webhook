9fddc520ee7a841d6a18dfa2925032aca057abf2
harisn5181 <91597885+harisn5181@users.noreply.github.com>
2021-11-11 02:22:35 +0530

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-BOT_TOKEN = "1908250775:AAFoQFc2_qQbl8VhVxwm9m88nsSwOQVNHyI"
-
-# Project Name of your Project
-PROJECT_NAME = "paradise_chat_bot"
-
-# If you forked the source code, grab your repo's link and paste it below!
-GIT_REPO_URL = "
-    https://paradisechat.herokuapp.com"
